c80db17bbd9fb892cffc76b6fc099adbf02a80e3
Xuan Hung Dao <luyendh@nal.vn>
2017-02-15 18:19:18 +0700

    Start project.

---
+++ src/assignment/java/view/ConsoleMenu.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.java.view;
+
+/**
+ *
+ * @author daolinh
+ */
+public class ConsoleMenu {
+    
+    public static void main(String[] args) {
+        
+    }
+}
